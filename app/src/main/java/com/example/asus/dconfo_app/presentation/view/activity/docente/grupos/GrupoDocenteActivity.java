a4df58d7ae6c796aa2c7072ef24ad7352604cfe1
maurocabor521 <37517603+maurocabor521@users.noreply.github.com>
2019-06-15 08:02:09 -0500

    nuevo commit

---
+++ app/src/main/java/com/example/asus/dconfo_app/presentation/view/activity/docente/grupos/GrupoDocenteActivity.java
@@ -0,0 +1,15 @@
+package com.example.asus.dconfo_app.presentation.view.activity.docente.grupos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.asus.dconfo_app.R;
+
+public class GrupoDocenteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grupo_docente2);
+    }
+}
