06d3115836b13778114a61bd54f4ee7305b3cfee
maurocabor521 <37517603+maurocabor521@users.noreply.github.com>
2019-07-10 21:03:52 -0500

    nuevo commit

--- app/src/main/java/com/example/asus/dconfo_app/presentation/view/fragment/docente/notas/FindNotasXEstudianteFragment.java
+++ app/src/main/java/com/example/asus/dconfo_app/presentation/view/fragment/docente/notas/FindNotasXEstudianteFragment.java
@@ -1,14 +1,46 @@
 package com.example.asus.dconfo_app.presentation.view.fragment.docente.notas;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.asus.dconfo_app.R;
+import com.example.asus.dconfo_app.domain.model.Curso;
+import com.example.asus.dconfo_app.domain.model.DeberEstudiante;
+import com.example.asus.dconfo_app.domain.model.Estudiante;
+import com.example.asus.dconfo_app.domain.model.VolleySingleton;
+import com.example.asus.dconfo_app.helpers.Globals;
+import com.example.asus.dconfo_app.presentation.view.adapter.NotasDeberesEstudianteAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,7 +50,8 @@ import com.example.asus.dconfo_app.R;
  * Use the {@link FindNotasXEstudianteFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FindNotasXEstudianteFragment extends Fragment {
+public class FindNotasXEstudianteFragment extends Fragment implements Response.Listener<JSONObject>,
+        Response.ErrorListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -27,6 +60,29 @@ public class FindNotasXEstudianteFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    int idgrupo;
+    int iddocente;
+    ProgressDialog progreso;
+
+    Spinner sp_Estudiantes;
+    EditText edt_Estudiante;
+    Button btn_BuscarEstudiante;
+    RecyclerView rv_datosEstudiante;
+    private int idestudiante;
+    private String flag;
+
+    ArrayList<String> listaStringIdGrupoEstudiantes;
+    ArrayList<Estudiante> listaEstudiantes;
+    ArrayList<Integer> listaIdEstudiantes;
+    List<String> listaStringEstudiantes = new ArrayList<>();
+    ArrayList<DeberEstudiante> listaDeberes_full;
+    ArrayList<Integer> lista_idEstudiante;
+
+    //RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
+    StringRequest stringRequest;
+
 
     private OnFragmentInteractionListener mListener;
 
@@ -66,8 +122,259 @@ public class FindNotasXEstudianteFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_find_notas_xestudiante, container, false);
+
+        progreso = new ProgressDialog(getActivity());
+        iddocente = getArguments().getInt("iddocente");
+        idgrupo = getArguments().getInt("idgrupo");
+
+        sp_Estudiantes = (Spinner) view.findViewById(R.id.sp_docente_estudiantes_nota);
+        edt_Estudiante = (EditText) view.findViewById(R.id.edt_doc_estudiante_nota);
+
+        rv_datosEstudiante = (RecyclerView) view.findViewById(R.id.rv_docente_X_Est_notas);
+        rv_datosEstudiante.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rv_datosEstudiante.setHasFixedSize(true);
+
+        listaDeberes_full = new ArrayList<>();
+        lista_idEstudiante = new ArrayList<>();
+        listaIdEstudiantes = new ArrayList<>();
+        listarEstudiantes();
+
+        btn_BuscarEstudiante = (Button) view.findViewById(R.id.btn_docente_buscar_nota);
+        btn_BuscarEstudiante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listaDeberes_full = new ArrayList<>();
+                lista_idEstudiante = new ArrayList<>();
+                listaIdEstudiantes = new ArrayList<>();
+                flag = "1";
+                cargarWebService();
+            }
+        });
+
         return view;
     }
+
+    private void cargarWebService() {
+
+        progreso.setMessage("Cargando...");
+        progreso.show();
+        // String ip = getString(R.string.ip);
+        //int iddoc=20181;
+        String iddoc = "20181";
+        String url_lh = Globals.url;
+
+        if (flag.equals("1")) {
+
+            String url = "http://" + url_lh + "/proyecto_dconfo_v1/8_5wsJSONConsultarListaDeberesEst_nota.php?estudiante_idestudiante=" + idestudiante;
+
+            url = url.replace(" ", "%20");
+            //hace el llamado a la url
+            jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, this, this);
+
+            final int MY_DEFAULT_TIMEOUT = 15000;
+            jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                    MY_DEFAULT_TIMEOUT,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+            // request.add(jsonObjectRequest);
+            VolleySingleton.getIntanciaVolley(getContext()).addToRequestQueue(jsonObjectRequest);//p21
+            //Toast.makeText(getApplicationContext(), "web service 1111", Toast.LENGTH_LONG).show();}
+        } else if (flag.equals("2")) {
+
+            String url = "http://" + url_lh + "/proyecto_dconfo_v1/8_5wsJSONConsultarListaDeberesEst_nota.php?estudiante_idestudiante=";
+
+            url = url.replace(" ", "%20");
+            //hace el llamado a la url
+            jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, this, this);
+
+            final int MY_DEFAULT_TIMEOUT = 15000;
+            jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                    MY_DEFAULT_TIMEOUT,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+            // request.add(jsonObjectRequest);
+            VolleySingleton.getIntanciaVolley(getContext()).addToRequestQueue(jsonObjectRequest);//p21
+            //Toast.makeText(getApplicationContext(), "web service 1111", Toast.LENGTH_LONG).show();}
+        }//flag="2"
+
+
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        progreso.hide();
+        Toast.makeText(getContext(), "No se puede cone , grupo doc" + error.toString(), Toast.LENGTH_LONG).show();
+        System.out.println();
+        Log.d("ERROR", error.toString());
+        progreso.hide();
+    }
+
+    // si esta bien el llamado a la url entonces entra a este metodo
+    @Override
+    public void onResponse(JSONObject response) {
+        progreso.hide();
+        if (flag.equals("1")) {
+            //Toast.makeText(getApplicationContext(), "Mensaje: " + response.toString(), Toast.LENGTH_SHORT).show();
+            DeberEstudiante deberEstudiante = null;
+            JSONArray json = response.optJSONArray("deber_nota1");
+
+
+            try {
+                for (int i = 0; i < json.length(); i++) {
+                    deberEstudiante = new DeberEstudiante();
+                    JSONObject jsonObject = null;
+                    jsonObject = json.getJSONObject(i);
+                    // jsonObject = new JSONObject(response);
+                    deberEstudiante.setIdEjercicio2(jsonObject.optInt("EjercicioG2_idEjercicioG2"));
+                    deberEstudiante.setIdEstudiante(jsonObject.optInt("estudiante_idestudiante"));
+                    deberEstudiante.setFechaDeber(jsonObject.optString("fechaestudiante_has_Deber"));
+                    deberEstudiante.setTipoDeber(jsonObject.optString("tipoDeber"));
+                    deberEstudiante.setIdDocente(jsonObject.optInt("docente_iddocente"));
+                    deberEstudiante.setIdCalificacion(jsonObject.optInt("calificacionestudiante_has_Deber"));
+                    deberEstudiante.setIdEstHasDeber(jsonObject.optInt("id_estudiante_has_Debercol"));
+                    listaDeberes_full.add(deberEstudiante);
+                    lista_idEstudiante.add(deberEstudiante.getIdEstudiante());
+
+                }
+                //Toast.makeText(getApplicationContext(), "listagrupos: " + listaGrupos.size(), Toast.LENGTH_LONG).show();
+                // Log.i("size", "lista: " + listaGrupos.size());
+                NotasDeberesEstudianteAdapter notasDeberesEstudianteAdapter = new NotasDeberesEstudianteAdapter(listaDeberes_full);
+
+                notasDeberesEstudianteAdapter.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+
+                    }
+                });
+                rv_datosEstudiante.setAdapter(notasDeberesEstudianteAdapter);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.d("error", response.toString());
+
+                Toast.makeText(getContext(), "No se ha podido establecer conexión: " + response.toString(), Toast.LENGTH_LONG).show();
+
+                progreso.hide();
+            }
+            // System.out.println("Lista id estudiante: " + lista_idEstudiante.toString());
+        }//flag="1"
+
+
+    }//********************************************
+
+
+    //***********************************
+    public void listarEstudiantes() {
+
+        String url_lh = Globals.url;
+
+        String url = "http://" + url_lh + "/proyecto_dconfo_v1/4wsJSONConsultarListaEstudiantesGrupoDocente.php?idgrupo=" + idgrupo;
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Do something with response
+                        //mTextView.setText(response.toString());
+
+                        Curso curso = null;
+                        Estudiante estudiante = null;
+
+
+                        ArrayList<Curso> listaDCursos = new ArrayList<>();
+                        //listaCursos1 = new ArrayList<>();
+
+                        listaEstudiantes = new ArrayList<>();
+
+                        // Process the JSON
+                        try {
+                            // Get the JSON array
+                            //JSONArray array = response.getJSONArray("students");
+                            JSONArray array = response.optJSONArray("grupo_h_e");
+
+                            // Loop through the array elements
+                            for (int i = 0; i < array.length(); i++) {
+                                // curso = new Curso();
+                                // JSONObject jsonObject = null;
+                                // jsonObject = json.getJSONObject(i);
+
+                                // Get current json object
+                                JSONObject student = array.getJSONObject(i);
+                                estudiante = new Estudiante();
+                                JSONObject jsonObject = null;
+                                jsonObject = array.getJSONObject(i);
+                                estudiante.setIdestudiante(jsonObject.optInt("estudiante_idestudiante"));
+                                estudiante.setNameestudiante(jsonObject.optString("nameEstudiante"));
+
+                                listaEstudiantes.add(estudiante);
+                            }
+
+                            listaStringEstudiantes = new ArrayList<>();
+                            // listaStringEstudiantes.add("Seleccione Id Estudiante");
+                            for (int i = 0; i < listaEstudiantes.size(); i++) {
+                                listaStringEstudiantes.add(listaEstudiantes.get(i).getIdestudiante().toString() + " - " + listaEstudiantes.get(i).getNameestudiante());
+                            }
+
+                            listaIdEstudiantes.add(0000);
+
+                            ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, listaStringEstudiantes);
+                            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            sp_Estudiantes.setAdapter(adapter);
+                            sp_Estudiantes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                                @Override
+                                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                                    if (position != -1) {
+                                        //listaIdEstudiantes.add(listaEstudiantes.get(position - 1).getIdestudiante());
+                                        listaIdEstudiantes.add(listaEstudiantes.get(position).getIdestudiante());
+                                        edt_Estudiante.setText(listaEstudiantes.get(position).getIdestudiante().toString());
+                                        idestudiante = Integer.parseInt(edt_Estudiante.getText().toString());
+                                        //System.out.println("lista id est: " + listaIdEstudiantes.toString());
+//                                        Toast.makeText(getApplicationContext(), "id est: " + listaIdEstudiantes.get(position), Toast.LENGTH_LONG).show();
+                                        //showListView();
+                                    } else {
+
+                                    }
+                                }
+
+                                @Override
+                                public void onNothingSelected(AdapterView<?> parent) {
+
+                                }
+                            });
+
+                            //Toast.makeText(getApplicationContext(), "lista estudiantes" + listaStringEstudiantes, Toast.LENGTH_LONG).show();
+                            System.out.println("estudiantes size: " + listaEstudiantes.size());
+                            System.out.println("estudiantes: " + listaEstudiantes.get(0).getIdestudiante());
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Do something when error occurred
+                        System.out.println();
+                        Log.d("ERROR: ", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(jsonObjectRequest);
+        //stringRequest.setRetryPolicy(new DefaultRetryPolicy(DefaultRetryPolicy.DEFAULT_TIMEOUT_MS * 2, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        //VolleySingleton.getIntanciaVolley(getApplicationContext()).addToRequestQueue(stringRequest);//p21
+    }
+
+    //***********************************
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
